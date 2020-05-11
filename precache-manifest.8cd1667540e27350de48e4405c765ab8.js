070594c967c4a83d29c8838b0660777def95bf32
Justin Lee <macintoshhd@MacBook-Pro.local>
2020-05-10 23:46:12 -0400

    Updates

---
+++ precache-manifest.8cd1667540e27350de48e4405c765ab8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d3dbc32cb50a935bd477976909da8f",
+    "url": "/My-robots/index.html"
+  },
+  {
+    "revision": "f3b6d61c3fa2756dfa2e",
+    "url": "/My-robots/static/css/main.c5166a07.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/My-robots/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-robots/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b6d61c3fa2756dfa2e",
+    "url": "/My-robots/static/js/main.4cc0e568.chunk.js"
+  },
+  {
+    "revision": "b0c1a5bc049b40f50335",
+    "url": "/My-robots/static/js/runtime-main.bc222cad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/My-robots/static/media/logo.5d5d9eef.svg"
+  }
+]);
